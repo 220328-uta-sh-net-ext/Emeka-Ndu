df1dd4a24f96707b39bd01171e51ec7e96677749
Nnaemeka Nduaguba <nduaguban@hotmail.com>
2022-05-05 19:43:59 -0400

    Still working..

---
+++ Project0-Restaurant/RestaurantModels/Restaurant.cs
@@ -0,0 +1,22 @@
+﻿namespace RestaurantModels;
+
+  public class Restaurant
+{
+    public string Name { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string Zipcode { get; set; }
+
+    public Restaurant()
+    {
+        Name = string.Empty;
+        City = string.Empty;
+        State = string.Empty;
+        Zipcode = string.Empty;
+    }
+}
+
+
+
+
+
